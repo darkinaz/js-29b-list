c98262c70f578fc1cdb0efd071f13489067c2d4c
Mantvydas <mantvydas.caplikas@gmail.com>
2021-07-13 12:41:47 +0300

    Style

---
+++ js/data/wordsData.js
@@ -0,0 +1,29 @@
+
+const wordsData  = [
+    {
+        en: 'dog',
+        lt: 'suo'
+    },
+    {
+        en: 'cat',
+        lt: 'kate'
+    },
+    {
+        en: 'kite',
+        lt: 'aitvaras'
+    },
+    {
+        en: 'sausage',
+        lt: 'desrele'
+    },
+    {
+        en: 'team',
+        lt: 'komanda'
+    },
+    {
+        en: 'you',
+        lt: 'tu'
+    },
+
+]
+export { wordsData}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import { wordsData } from "./data/wordsData.js";
+import { Dictionary } from "./components/dictionary.js";
+
+// console.log(wordsData);
+// console.log(Dictionary);
+
+const zodynas = new Dictionary('.content', 'Anglu-Lietuviu-kalbos-zodynas', wordsData);
+
+console.log(zodynas);
